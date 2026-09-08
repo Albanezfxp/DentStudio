512a373f6754b5507e5cd9a55846856cb518e046
Albanezfxp <gabrielalbanez2701@gmail.com>
2026-09-08 17:39:08 -0300

    feat: add espac

--- src/data/doctors.ts
+++ src/data/doctors.ts
@@ -74,7 +74,7 @@ export const doctors: Doctor[] = [
     focus: "Harmonização Orofacial",
     facts: [
       { label: "Registro", value: "CRO-PB 12353" },
-      { value: "Harmonização Orofacial \n Lentes em resina" },
+      { value: "Harmonização Orofacial e Lentes em resina" },
     ],
     whatsapp: "https://wa.me/5583993897637",
     whatsappLabel: "(83) 99389-7637",
